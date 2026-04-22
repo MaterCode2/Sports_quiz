21e26bc6b6fe832c2763c377632f2676888949ee
MaterCode2 <lavina@stu.otc.school.nz>
2026-04-22 15:18:32 +1200

    Added 2 Questions

--- Quiz.py
+++ Quiz.py
@@ -6,12 +6,13 @@ print("This is a quiz on New Zealand sports star, Lisa Carrignton")
 print("The quiz will have a combination of multi-choice and short answer questions.")
 
 #Creates a dictionary of 10 questions and answers
-QandA = {"How many Oympic gold medals has Lisa Carrington won?" : 8,
-         "What is the name of Lisa Carringon's dog?\na) Max, b) Jack, c) Colin" : "a",
-         "In what year did Lisa Carrington become a Dame?" : 2022,
-         "In what city's Olympics did Lisa Carrington win her first gold medal" : "london",
-         "True or False, Lisa Carrington has broken multiple kayaking records." : "true",
-         "What date is Lisa Carrington's birthday?\na) 12th January, b) 23rd June, c) 20th October" : "c",
-         "How tall is Lisa Carrington?"
-         
+QandA = {"How many Oympic gold medals has Lisa Carrington won? " : 8,
+         "What is the name of Lisa Carringon's dog?\na) Max, b) Jack, c) Colin " : "a",
+         "In what year did Lisa Carrington become a Dame? " : 2022,
+         "In what city's Olympics did Lisa Carrington win her first gold medal " : "london",
+         "True or False, Lisa Carrington has broken multiple kayaking records. " : "true",
+         "What date is Lisa Carrington's birthday?\na) 12th January, b) 23rd June, c) 20th October " : "c",
+         "How tall is Lisa Carrington? (In metres to 1dp) " : 1.7,
+         "What city was Lisa Carrington born in? " : "tauranga",
+         "True or False, Lisa Carrington is NZ's second most decorated olympian. " : "false"
          }
