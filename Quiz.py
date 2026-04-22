ddb08873ee4145568f084ed24becea053a787e10
MaterCode2 <lavina@stu.otc.school.nz>
2026-04-22 15:08:26 +1200

    Initial Push

---
+++ Quiz.py
@@ -0,0 +1,17 @@
+'''This code gives users a quiz about New Zealand sports star, Lisa Carrington.
+By Abigail Lavin - Created on 20/4/2026'''
+
+#Creates a user friendly statement to introduce the quiz
+print("This is a quiz on New Zealand sports star, Lisa Carrignton")
+print("The quiz will have a combination of multi-choice and short answer questions.")
+
+#Creates a dictionary of 10 questions and answers
+QandA = {"How many Oympic gold medals has Lisa Carrington won?" : 8,
+         "What is the name of Lisa Carringon's dog?\na) Max, b) Jack, c) Colin" : "a",
+         "In what year did Lisa Carrington become a Dame?" : 2022,
+         "In what city's Olympics did Lisa Carrington win her first gold medal" : "london",
+         "True or False, Lisa Carrington has broken multiple kayaking records." : "true",
+         "What date is Lisa Carrington's birthday?\na) 12th January, b) 23rd June, c) 20th October" : "c",
+         "How tall is Lisa Carrington?"
+         
+         }
